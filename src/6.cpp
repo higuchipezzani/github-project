e593349fe83d2c62f720f6f31a9014e127233ec0
higuchipezzani <higuchipezzani@gmail.com>
2025-03-10 11:33:46 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,10 @@
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x < y) {
+        cout << "x is less than y" << endl;
+    } else {
+        cout << "x is greater than or equal to y" << endl;
+    }
+    return 0;
+}
