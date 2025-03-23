0a5b2cca3609ec4bf9683c811079092c067f5afd
higuchipezzani <higuchipezzani@gmail.com>
2025-03-23 05:09:49 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    
+    return 0;
+}
